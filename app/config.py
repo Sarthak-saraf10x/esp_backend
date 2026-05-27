8a96d97bb2fee7274aea7639c642608924609b24
sarthaksara <sarthaksaraf.10x@gmail.com>
2026-05-27 21:18:37 +0530

    change config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AIzaSyBbxn4Y5xzS9RgNSV6FtTjjl63wJopMMSs")
+    GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
     WHISPER_MODEL_NAME = "base.en"
     PIPER_VOICE_PATH = "./en_US-lessac-medium.onnx"
     MCP_SERVER_SCRIPT = "./mcp_server.py"
